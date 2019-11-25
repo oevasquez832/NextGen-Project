39bfd663b6a91dcdeaf2ab7e0e75eb3a1b0d4ee9
oevasquez832 <oevasquez832@gmail.com>
2019-11-24 19:15:48 -0600

    Importing screen access to App js

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { createSwitchNavigator,createStackNavigator} from 'react-navigation'
 import AuthLoadingScreen from './src/components/screens/AuthLoadingScreen'
+// screen imports
+import WelcomeScreen from './src/components/screens/WelcomeScreen'
+import SignUpScreen from './src/components/screens/SignUpScreen'
+import SignInScreen from './src/components/screens/SignInScreen'
+import ForgetPasswordScreen from './src/components/screens/ForgetPasswordScreen'
 // Auth stack
 const AuthStackNavigator = createStackNavigator({
   Welcome: {
