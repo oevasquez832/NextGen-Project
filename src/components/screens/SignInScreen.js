31c970d8dff9b752a23d445f4c4b99e6487367aa
oevasquez832 <oevasquez832@gmail.com>
2019-11-24 18:46:10 -0600

    Added template stylesheets to Sign In, Sign Up, and Forgetting Password

---
+++ src/components/screens/SignInScreen.js
@@ -0,0 +1,22 @@
+import {
+    StyleSheet,
+    View,
+    Text,
+  } from 'react-native'
+  export default class ScreenName extends React.Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text>Sign In</Text>
+        </View>
+      )
+    }
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#aa73b7',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  })
