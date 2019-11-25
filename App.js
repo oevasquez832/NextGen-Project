016f99e63d0338cc95fd09f3dc905c0a95529b7e
oevasquez832 <oevasquez832@gmail.com>
2019-11-24 18:42:56 -0600

    committing changes to App js

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { createSwitchNavigator } from 'react-navigation'
+import { createSwitchNavigator,createStackNavigator} from 'react-navigation'
 import AuthLoadingScreen from './src/components/screens/AuthLoadingScreen'
+// Auth stack
+const AuthStackNavigator = createStackNavigator({
+  Welcome: {
+    screen: WelcomeScreen,
+    navigationOptions: () => ({
+      title: `Welcome to this App`, // for the header screen
+      headerBackTitle: 'Back'
+    }),
+  },
+  SignUp: {
+    screen: SignUpScreen,
+    navigationOptions: () => ({
+      title: `Create a new account`,
+    }),
+  },
+  SignIn: {
+    screen: SignInScreen,
+    navigationOptions: () => ({
+      title: `Log in to your account`,
+    }),
+  },
+  ForgetPassword: {
+    screen: ForgetPasswordScreen,
+    navigationOptions: () => ({
+      title: `Create a new password`,
+    }),
+  },
+})
 export default createSwitchNavigator({
   // screen: name
-  AuthLoading: AuthLoadingScreen
+  AuthLoading: AuthLoadingScreen,
+  Auth: AuthStackNavigator, //Auth stack
 })
 
 const styles = StyleSheet.create({
